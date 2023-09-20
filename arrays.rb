8eb1f681ba3b7e446e3bcfc92b0b29d6c2c85c2d
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-09-20 11:16:35 -0600

    Add arrays of different data types

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+puts "Array Practice"
+
+guitars = ["SG", "Les Paul", "Strat", "Telecaster", "Toronado"]
+
+favorite_numbers = [7, 343, 21, 1111]
+
+random_floats = [4.56, 5.67, 6.78, 7.89]
+
+boobabies = [true, false, true , false]
+
