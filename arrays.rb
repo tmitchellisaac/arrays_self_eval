2a4a9e1f10a987cb326d44a9aa43e9f36a6de0e2
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-09-20 11:25:11 -0600

    Calls newly learned methods on my arrays

--- arrays.rb
+++ arrays.rb
@@ -8,3 +8,19 @@ random_floats = [4.56, 5.67, 6.78, 7.89]
 
 boobabies = [true, false, true , false]
 
+
+# This will permanently delete the last element in the array `guitars`
+# (Toronado)
+guitars.pop 
+
+# This will permanently remove the first element 
+# in the array `favorite_numbers`
+favorite_numbers.shift
+
+# This will add the three additional elements to the end of the array
+random_floats.push(3.45, 2.34, 1.23)
+
+# This will add elements to the beginning of the array `boobabies`
+boobabies.unshift(false, false, false)
+
+
